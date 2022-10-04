23d725721ba26d33cda04fc59a72fe8262894b13
Praveen Yaragatti <57564746+YaragattiPraveen@users.noreply.github.com>
2022-10-04 14:36:48 +0530

    Initial Commit
    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [search, setsearch] = useState();
+  const [foodlist, setfoodlist] = useState([])
+
+  const url = `https://api.edamam.com/search?q=${search}&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}&to=32`
+
+  const fetchFoodRecipe = async () => {
+    try {
+      const data = await axios.get(url);
+      setfoodlist(data.data.hits)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchFoodRecipe() // eslint-disable-next-line 
+  }, [])
+
+
+  function submit(e) {
+    e.preventDefault()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Food Recipe App</h1>
+      <form onSubmit={submit}>
+        <input type='text' className='inputSearch' placeholder='Search with ingredients..' onChange={(e) => setsearch(e.target.value)} />
+        <button onClick={fetchFoodRecipe} onKeyPress={(e) => {
+          if (e.key === 'Enter') {
+            fetchFoodRecipe()
+            setsearch('')
+          }
+        }}>Search</button>
+      </form>
+      <div className='food_item_container'>
+        {
+          foodlist.map((val, index) => {
+            return (
+              <div className='card' key={index}>
+                <img src={val.recipe.image} alt='random img' />
+                <h3>{val.recipe.label}</h3>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
